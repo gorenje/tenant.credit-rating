f5c3fbda226f4fce140e8acbf213a5946737c27a
Gerrit Riessen <gerrit.riessen@gmail.com>
2017-04-27 16:56:36 +0200

    using bootstrap and added add account

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+MenuItems = {
+  "Konten" => "/accounts",
+  "Bonität" => "/rating",
+  "Dienste" => "/services",
+  "Logout" => "/logout"
+}
+
 FigoPublicKey="-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1qB2hmObbCbAM+lc+ggDauoIZReejEimnvrmeqEs0opeTeZiiietoHT1FkB8HjlgCWrh6UimxrRvBwwvNn/4uiVEqxuPb37ozWRj87bp1R3iwhzIGHBMgkibfFf9v3FxEjtY6CgCvOJ/12+AiotL+4jBCwsUWcqui3phq4/C19bQTWaN8u1Q1ABB0SSExcfqH3Ahg6i4pJfDwY+/khb4rgvmqPpb7a0tHiWuWqAMUxfEO/GJVaDV+Bq4k5vfUNirIcazUtmnLhBVSTBcjw7OEDEIHGckwUHs6prKE0kkQD4Xjm06XupuZW8/H+/oPBdHJBr+Ugv5Kzlsst/81BEyoQIDAQAB-----END PUBLIC KEY-----"
 
 RatingFactors = {
--- routes/user.rb
+++ routes/user.rb
@@ -1,8 +1,17 @@
 get '/add_account' do
-  haml :add_account, :layout => false
+  redirect '/' if session[:user_id].nil?
+
+  haml :add_account
 end
 
 post '/add_account' do
-  puts params
-  haml :add_account, :layout => false
+  redirect '/' if session[:user_id].nil?
+
+  @user = User.find(session[:user_id])
+  if params[:creds_type] == "account"
+    @user.figo_session.add_account("DE", params[:creds], "90090042",
+                                   "DE67900900424711951500", true)
+  else
+  end
+  haml :add_account
 end
