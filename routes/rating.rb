1f5ec0b97ba80ba1198efbf55cc976ee2981c8c9
Gerrit Riessen <gerrit.riessen@gmail.com>
2017-05-03 12:50:17 +0200

    added a user rating page for the badge

--- routes/rating.rb
+++ routes/rating.rb
@@ -1,7 +1,10 @@
+get '/rating/:eid' do
+  @user = User.find_by_external_id(params[:eid])
+  haml :rating_user
+end
+
 get '/rating' do
   redirect '/' if session[:user_id].nil?
-
   @user = User.find(session[:user_id])
-
   haml :rating
 end
