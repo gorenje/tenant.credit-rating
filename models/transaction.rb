150fbb528adbb65dfe5299c1bf3ef19adecded4e
Gerrit Riessen <gerrit.riessen@gmail.com>
2017-05-05 17:32:12 +0200

    added 2nd graph

--- models/transaction.rb
+++ models/transaction.rb
@@ -2,6 +2,18 @@ class Transaction < ActiveRecord::Base
   belongs_to :account
 
   self.inheritance_column = :classname
+
+  def credit?
+    amount.to_f >= 0
+  end
+
+  def debit?
+    amount.to_f < 0
+  end
+
+  def atm?
+    name =~ /^GA/
+  end
 end
 
 class FigoTransaction < Transaction
--- routes/api.rb
+++ routes/api.rb
@@ -1,21 +1,75 @@
 get '/api/account/:account_id.json' do
   return_json do
     if is_logged_in?
-      xcnt, xlookup  = 0,{}
-      data = get_account.transactions.group_by do |transaction|
+      xcnt, xlookup  = -1,{}
+      data = [{ "name" => "Ingoing",  "color" => "green",     "data" => [], },
+              { "name" => "Outgoing", "color" => "red",       "data" => [], },
+              { "name" => "Total",    "color" => "steelblue", "data" => [] }]
+
+      get_account.transactions.group_by do |transaction|
         transaction.booking_date.strftime("%Y%m")
       end.sort_by { |a,_| a.to_i }.map do |month, transactions|
-        { "x" => xcnt,
-          "y" => transactions.map {|t| t.amount.to_f}.sum.round(2)}.tap do
-          xlookup[xcnt] ="%s/%s" % [month[4..6], month[0..3]]
-          xcnt+=1
-        end
+        xcnt+=1
+        xlookup[xcnt] = "%s/%s" % [month[4..6], month[0..3]]
+
+        data[0]["data"] << { "x" => xcnt,
+          "y" => (transactions.select(&:credit?).
+                  map { |t| t.amount.to_f }.sum.round(2))
+        }
+        data[1]["data"] << { "x" => xcnt,
+          "y" => (transactions.select(&:debit?).
+                  map { |t| t.amount.to_f }.sum.round(2))
+        }
+        data[2]["data"] << { "x" => xcnt,
+          "y" => transactions.map {|t| t.amount.to_f}.sum.round(2)
+        }
       end
-      values = data.map { |h| h["y"] }
+
+      values = data.map { |d| d["data"].map { |e| e["y"] }}.flatten
       { :data    => data,
         :xlookup => xlookup,
-        :ymax    => values.max + 100,
-        :ymin    => values.min - 100
+        :ymax    => values.max + 500,
+        :ymin    => values.min - 500
+      }
+    else
+      []
+    end
+  end
+end
+
+get '/api/account/atm/:account_id.json' do
+  return_json do
+    if is_logged_in?
+      xcnt, xlookup  = -1,{}
+      data = [{ "name" => "Ingoing",  "color" => "green",     "data" => [], },
+              { "name" => "Outgoing", "color" => "red",       "data" => [], },
+              { "name" => "Total",    "color" => "steelblue", "data" => [] }]
+
+      get_account.transactions.select(&:atm?).
+        group_by do |transaction|
+        transaction.booking_date.strftime("%Y%m")
+      end.sort_by { |a,_| a.to_i }.map do |month, transactions|
+        xcnt+=1
+        xlookup[xcnt] = "%s/%s" % [month[4..6], month[0..3]]
+
+        data[0]["data"] << { "x" => xcnt,
+          "y" => (transactions.select(&:credit?).
+                  map { |t| t.amount.to_f }.sum.round(2))
+        }
+        data[1]["data"] << { "x" => xcnt,
+          "y" => (transactions.select(&:debit?).
+                  map { |t| t.amount.to_f }.sum.round(2))
+        }
+        data[2]["data"] << { "x" => xcnt,
+          "y" => transactions.map {|t| t.amount.to_f}.sum.round(2)
+        }
+      end
+
+      values = data.map { |d| d["data"].map { |e| e["y"] }}.flatten
+      { :data    => data,
+        :xlookup => xlookup,
+        :ymax    => values.max + 500,
+        :ymin    => values.min - 500
       }
     else
       []
